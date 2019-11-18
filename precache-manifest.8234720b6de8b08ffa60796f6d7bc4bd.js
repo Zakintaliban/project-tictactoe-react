9dbad9b4df81a2b5ebad337c37973d21886e507c
zakintaliban <nukmans23@gmail.com>
2019-11-18 10:45:46 +0700

    Updates

---
+++ precache-manifest.8234720b6de8b08ffa60796f6d7bc4bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a05b3a31c9d3004fddad18c7f0a1fbe",
+    "url": "/project-tictactoe-react/index.html"
+  },
+  {
+    "revision": "c90c21844ad15a7a0d0d",
+    "url": "/project-tictactoe-react/static/css/main.529cbd99.chunk.css"
+  },
+  {
+    "revision": "0ca9d43d04fcea768579",
+    "url": "/project-tictactoe-react/static/js/2.b3e34ae7.chunk.js"
+  },
+  {
+    "revision": "c90c21844ad15a7a0d0d",
+    "url": "/project-tictactoe-react/static/js/main.cd0c8313.chunk.js"
+  },
+  {
+    "revision": "70b11d16944d9998f9ab",
+    "url": "/project-tictactoe-react/static/js/runtime-main.4ae04f75.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/project-tictactoe-react/static/media/logo.25bf045c.svg"
+  }
+]);
